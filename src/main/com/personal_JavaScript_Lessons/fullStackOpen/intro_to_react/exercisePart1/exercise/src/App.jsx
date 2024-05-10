8a92a5a3e4086ada1db79b5a1af81e6dcde9fa5d
Vasili2797 <nashvilliv1@gmail.com>
2024-05-10 05:32:52 -0400

    refactored according to the requirements

--- src/main/com/personal_JavaScript_Lessons/fullStackOpen/intro_to_react/exercisePart1/exercise/src/App.jsx
+++ src/main/com/personal_JavaScript_Lessons/fullStackOpen/intro_to_react/exercisePart1/exercise/src/App.jsx
@@ -1,31 +1,35 @@
 function App() {
   const course = "Half Stack application development";
-
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   const Part = (props) => {
     return (
-      <p>
+      <div>
         {props.name} {props.exercises}
-      </p>
+      </div>
     );
   };
 
   const TotalExercises = () => {
     let sum = 0;
-    sum += part1.exercises + part2.exercises + part3.exercises;
+
+    parts.map((element) => {
+      sum += element.exercises;
+    });
+
     let returningString = `Number of exercises ${sum}`;
     return <p>{returningString}</p>;
   };
@@ -33,11 +37,13 @@ function App() {
   return (
     <div>
       <h1>{course}</h1>
-      <Part name={part1.name} exercises={part1.exercises} />
-
-      <Part name={part2.name} exercises={part2.exercises} />
-
-      <Part name={part3.name} exercises={part3.exercises} />
+      <Part
+        name={parts.map((element) => (
+          <p key={element.name}>
+            {element.name} {element.exercises}
+          </p>
+        ))}
+      />
       <TotalExercises />
     </div>
   );
